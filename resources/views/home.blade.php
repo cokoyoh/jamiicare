4f2ba82f3d2658c6e5e4b40d5944312757e2f6f4
cokoyoh <charlesokoyoh@gmail.com>
2019-01-16 10:55:49 +0300

    Add bootswatch theme and add modal for adding appoitmnets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,74 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">User Dashboard</div>
+                {{--<div class="card-body">--}}
+                {{--</div>--}}
+            </div>
+            <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Checkups
+                    <span class="badge badge-primary badge-pill">2</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Surgery
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Surgery
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Maternal Health
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Maternal Health
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                     Dental
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Opticial
+                    <span class="badge badge-primary badge-pill">1</span>
+                </li>
+            </ul>
+        </div>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Appointments
+                    <button type="button"
+                            class="btn btn-sm btn-outline-success"
+                            data-toggle="modal" data-target="#addAppointmentModal"
+                    >
+                        Add Appointment
+                    </button>
+                </div>
+
+                <div class="modal" id="addAppointmentModal">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Book Appointment</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                {{--<p>Modal body text goes here.</p>--}}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-outline-info">Submit</button>
+                                <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +78,26 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Doctor</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">Default</th>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                                <td>Column content</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+
                 </div>
             </div>
         </div>
